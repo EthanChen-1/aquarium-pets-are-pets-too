3016c9a7c2d6579560fdba742263c5499812dbab
Ethan Chen <Ethanchen4@gmail.com>
2025-04-14 23:45:45 -0400

    changed overall layout of application

--- frontend/src/components/hero/hero.jsx
+++ frontend/src/components/hero/hero.jsx
@@ -3,10 +3,13 @@ import styles from "./hero.module.css";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h2>
-        Your aquatic pets aren't a piece of decor, they're family just like your
-        fluffier friends. So let's treat them with the same love and care.
-      </h2>
+      <div className={styles["hero-flex"]}>
+        <h2>
+          Your aquatic pets aren't a piece of decor, they're family just like
+          your fluffier friends. So let's treat them with the same love and
+          care.
+        </h2>
+      </div>
       <ul className={styles.bubbles}>
         <li></li>
         <li></li>
